62d90f727dea919d5156e434cb872bec868f03cd
Scathies <Scathiesmail@gmail.com>
2024-03-31 15:11:56 +0300

    add: migration; sql-scripts for db and users table

---
+++ src/main/resources/db/changelog/v.1.0.0/31.01.2024.create-db.sql
@@ -0,0 +1,4 @@
+--liquibase formatted sql
+
+--changeset scathies:31.01.2024.create-db
+CREATE DATABASE cloud_file_storage;
---
+++ src/main/resources/db/changelog/v.1.0.0/31.01.2024.create-table-users.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset scathies:31.01.2024.create-table-users
+CREATE TABLE users
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(128) UNIQUE NOT NULL,
+    password VARCHAR(128)        NOT NULL
+);
